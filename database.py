c11bb3503b55812a67079c38f0b9d3a1a2c0f35a
Mauricio Oliveira <mauriciocasoli@gmail.com>
2026-06-11 13:20:25 -0400

    improved db logic

--- database.py
+++ database.py
@@ -7,106 +7,87 @@ def connect():
 
 
 def create_tables():
-    con = connect()
-    cur = con.cursor()
-
-    cur.execute(
-        "CREATE TABLE IF NOT EXISTS master ("
-        "id INTEGER primary key,"
-        "master_password TEXT NOT NULL,"
-        "salt TEXT NOT NULL"
-        ")"
-    )
-    con.commit()
-
-    cur.execute(
-        "CREATE TABLE IF NOT EXISTS passwords ("
-        "id INTEGER primary key,"
-        "service TEXT NOT NULL,"
-        "username TEXT NOT NULL,"
-        "password TEXT NOT NULL"
-        ")"
-    )
-    con.commit()
-
-    con.close()
+    with connect() as con:
+        cur = con.cursor()
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS master ("
+            "id INTEGER primary key,"
+            "master_password TEXT NOT NULL,"
+            "salt TEXT NOT NULL"
+            ")"
+        )
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS passwords ("
+            "id INTEGER primary key,"
+            "service TEXT NOT NULL,"
+            "username TEXT NOT NULL,"
+            "password TEXT NOT NULL"
+            ")"
+        )
+        con.commit()
 
 
 def check_master_password():
-    con = connect()
-    cur = con.cursor()
-
-    cur.execute("SELECT master_password FROM master")
-    try:
-        master_password = cur.fetchone()[0]
-        return master_password
-    except:
-        return False
+    with connect() as con:
+        cur = con.cursor()
+        cur.execute("SELECT master_password FROM master")
+        try:
+            master_password = cur.fetchone()[0]
+            return master_password
+        except:
+            return False
 
 
 def create_master_password(password, salt):
-    con = connect()
-    cur = con.cursor()
-
-    cur.execute(
-        "INSERT INTO master (master_password, salt) VALUES (?, ?)", (password, salt)
-    )
-
-    con.commit()
-    con.close()
+    with connect() as con:
+        cur = con.cursor()
+        cur.execute(
+            "INSERT INTO master (master_password, salt) VALUES (?, ?)", (password, salt)
+        )
+        con.commit()
 
 
 def read_table():
-    con = connect()
-    cur = con.cursor()
-    cur.execute("SELECT id, service, username FROM passwords")
-    rows = cur.fetchall()
-    con.close()
-    return rows
+    with connect() as con:
+        cur = con.cursor()
+        cur.execute("SELECT id, service, username FROM passwords")
+        rows = cur.fetchall()
+        return rows
 
 
 def get_password(id):
-    con = connect()
-    cur = con.cursor()
-    cur.execute("SELECT password FROM passwords WHERE id = (?)", (id,))
-    password = cur.fetchone()[0]
-    return password
+    with connect() as con:
+        cur = con.cursor()
+        cur.execute("SELECT password FROM passwords WHERE id = (?)", (id,))
+        password = cur.fetchone()[0]
+        return password
 
 
 def add_password(service, username, password):
-    con = connect()
-    cur = con.cursor()
-
-    cur.execute(
-        "INSERT INTO passwords (service, username, password) VALUES (?, ?, ?)",
-        (service, username, password),
-    )
-
-    con.commit()
-    con.close()
+    with connect() as con:
+        cur = con.cursor()
+        cur.execute(
+            "INSERT INTO passwords (service, username, password) VALUES (?, ?, ?)",
+            (service, username, password),
+        )
+        con.commit()
 
 
 def get_salt():
-    con = connect()
-    cur = con.cursor()
-    cur.execute("SELECT salt FROM master")
+    with connect() as con:
+        cur = con.cursor()
+        cur.execute("SELECT salt FROM master")
+        salt = cur.fetchone()[0]
+        return salt
 
-    salt = cur.fetchone()[0]
-    con.close()
-
-    return salt
 
 def exist_master_user():
-    con = connect()
-    cur = con.cursor()
-    cur.execute("SELECT * FROM master")
-
-    try:
-        row = cur.fetchone()
-        return row is not None
-    except Exception as e:
-        print(f"Error: {e}")
-        return False
-    finally:
-        con.close()
-
+    with connect() as con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM master")
+        try:
+            row = cur.fetchone()
+            return row is not None
+        except Exception as e:
+            print(f"Error: {e}")
+            return False
